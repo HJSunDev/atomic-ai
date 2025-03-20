2d3edbb3153540b05bfde88261055576fc02627e
HJSunDev <13720980943@126.com>
2025-03-20 23:59:28 +0800

    feat(DashboardPage): add HopeBlock component

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,10 +3,12 @@
 import { DashboardPageWrapper } from "./_components/DashboardPageWrapper";
 import { AtomicBlock } from "./_components/AtomicBlock";
 import { TestBlock } from "./_components/TestBlock";
+import { HopeBlock } from "./_components/HopeBlock";
 
 export default function DashboardPage() {
   return (
     <DashboardPageWrapper>
+      <HopeBlock />
       <TestBlock />
       <div className="space-y-6">
         {/* 欢迎区域 */}
