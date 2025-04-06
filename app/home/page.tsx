4e310fe3ec6d58280c916b050be58212fb4fed86
HJSunDev <13720980943@126.com>
2025-04-06 23:14:14 +0800

    feat(DashboardPage): add NewBlock component to dashboard layout

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,10 +4,12 @@ import { DashboardPageWrapper } from "./_components/DashboardPageWrapper";
 import { AtomicBlock } from "./_components/AtomicBlock";
 import { TestBlock } from "./_components/TestBlock";
 import { HopeBlock } from "./_components/HopeBlock";
+import { NewBlock } from "./_components/NewBlock";
 
 export default function DashboardPage() {
   return (
     <DashboardPageWrapper>
+      <NewBlock />
       <HopeBlock />
       <TestBlock />
       <div className="space-y-6">
